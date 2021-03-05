7478f98ebf8f48b176b606df8932cc6a9d320e7f
Simon Brunning <simon@brunningonline.net>
2021-03-05 11:53:08 +0000

    Ugly, but kinda working.

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 Spike for a Mac menu bar app to display Github actions run status.
 
+This is **very** much spike code - no tests, hard-coding everywhere, and the structure is all wrong. Kinda works, though.
+
 To run:
 
     python3 -m venv venv
--- status.py
+++ status.py
@@ -1,34 +1,83 @@
+from dataclasses import dataclass
+
 import rumps
 import requests
 import webbrowser
+from furl import furl
+from box import Box
 
 rumps.debug_mode(True)
 
-OK = "✅"
-FAILED = "❌"
-repos = [('brunns', 'mbtest'), ('brunns', 'brunns-matchers')]
+OK = "🟢"
+FAILED = "🔴"
+RUNNING = "🟠"
+REPOS = [('brunns', 'mbtest'), ('brunns', 'brunns-matchers')]
+
+
+def main():
+    app = StatusApp()
+
+    for owner, name in REPOS:
+        menu_item = rumps.MenuItem(f"{owner}/{name}")
+        repo = Repo(owner, name, menu_item)
+        repo.menu_item.set_callback(repo.on_click)
+        app.add(repo)
+
+    app.run()
+
+    @rumps.timer(90 * len(REPOS))
+    def check(self):
+        for repo in app.repos:
+            repo.check()
+
+        status = OK if all(repo.conclusion == "success" for repo in app.repos) else FAILED
+        app.app.title = status
+        if status == FAILED: rumps.notification(title="Oooops...", subtitle="You fucked it up again.", message="Now go and fix it.")
+
+
+@dataclass
+class Repo:
+    owner: str
+    repo: str
+    menu_item: rumps.MenuItem
+    conclusion: str = None
+    url: str = None
+
+    def check(self):
+        run = self.get_run()
+        self.url = run.html_url
+        self.conclusion = run.conclusion
+        state = OK if run.conclusion == "success" else FAILED
+        self.menu_item.title = f"{state} {self.owner}/{self.repo}"
+
+    def get_run(self):
+        resp = requests.get(self.github_api_url())
+        resp.raise_for_status()
+        return Box(resp.json()['workflow_runs'][0])
+
+    def github_api_url(self):
+        url = furl("https://api.github.com/repos/") / self.owner / self.repo / "actions/runs"
+        url.args['accept'] = "application/vnd.github.v3+json"
+        url.args['status'] = "completed"
+        url.args['per_page'] = 1
+        return url
+
+    def on_click(self, foo):
+        if self.url: webbrowser.open(self.url)
 
 
 class StatusApp:
     def __init__(self):
-        self.app = rumps.App("Gitb Actions Status", OK)
+        self.app = rumps.App("Github Actions Status", OK)
+        self.repos = []
 
     def run(self):
         self.app.run()
-        rumps.notification(title="Starting...", subtitle="", message="")
-        print("starting")
 
-    @rumps.timer(60)
-    def check(self):
-        rumps.notification(title="Checking...", subtitle="", message="")
-        print("checking")
-        for owner, repo in repos:
-            url = f"https://api.github.com/repos/{owner}/{repo}/actions/runs"
-            r = requests.get(url)
-            for run in r.json()['workflow_runs'][:9]:
-                print(owner, repo, run['status'], run['conclusion'], run['created_at'], run['html_url'])
+    def add(self, repo):
+        self.repos.append(repo)
+        self.app.menu.add(repo.menu_item)
 
 
 if __name__ == "__main__":
-    app = StatusApp()
-    app.run()
+    main()
