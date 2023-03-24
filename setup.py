b77821164cce35ac96420015e198be1f140861bf
Simon Brunning <simon@brunningonline.net>
2023-03-24 15:40:42 +0000

    Bump dependency versions.

--- setup.py
+++ setup.py
@@ -5,15 +5,15 @@ from setuptools import setup
 APP = ["status.py"]
 DATA_FILES = []
 DEPENDENCIES = [
-    "requests==2.28.1",
+    "requests==2.28.2",
     "rumps==0.4.0",
     "furl==2.1.3",
-    "python-box==6.1.0",
-    "ordered-enum==0.0.6",
+    "python-box==7.0.1",
+    "ordered-enum==0.0.8",
     "arrow==1.2.3",
     "contexttimer==0.3.3",
-    "humanize==4.4.0",
-    "python-json-logger==2.0.4",
+    "humanize==4.6.0",
+    "python-json-logger==2.0.7",
     "pyperclip==1.8.2",
 ]
 OPTIONS = {
